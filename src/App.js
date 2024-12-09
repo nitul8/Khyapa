182f3de2eface6dadf1ccd2ce25a42569316af96
Nitul Das <ndas6732@gmail.com>
2024-12-09 17:24:10 +0530

    new created

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div className="w-full h-screen flex justify-center items-center font-bold text-6xl">
+            Hello
+        </div>
+    );
+};
+
+export default App;
